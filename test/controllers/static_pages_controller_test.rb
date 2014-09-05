025c5a8473a029a235c65716420cca684f64e259
Anna McKelvey <anna.e.mckelvey@gmail.com>
2014-09-05 11:28:48 -0400

    Add Bootstrap

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "NewCo | Home"
+    assert_select "title", "NewCo"
   end
 
 
