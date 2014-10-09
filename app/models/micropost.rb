7e67cfe09effc87de2c400939a79103d900acc7e
Anna McKelvey <anna.e.mckelvey@gmail.com>
2014-10-09 12:55:14 -0400

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,15 @@ class Micropost < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 340 }
+  validate :picture_size
+
+  # Returns microposts from the users being followed by the given user.
+  def Micropost.from_users_followed_by(user)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    where("user_id IN (#{following_ids}) OR user_id = :user_id",
+          user_id: user)
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -19,11 +19,16 @@ Rails.application.routes.draw do
   delete 'logout'   => 'sessions#destroy' # should be invoked using an HTTP DELETE request
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   
 
